893e6e9706acf9cfa3455164968a70f11ed92ad3
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-02-11 12:21:33 -0300

    refactors handlebars code to nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -4,13 +4,10 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/img");
 	eleventyConfig.addPassthroughCopy("./src/fonts");
 
-	eleventyConfig.addHandlebarsHelper('json', function(context) {
-		return JSON.stringify(context);
-	});
-
 	return {
 		dir: {
 			input: "src",
+			includes: "includes",
 			output: "public",
 		},
 	};
